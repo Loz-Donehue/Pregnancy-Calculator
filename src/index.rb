71b65a65f241139c970a42ad68d0fc30029c0a71
Loz-Donehue <12014@coderacademy.edu.au>
2022-04-22 11:57:20 +1000

    Attempting to make a class called interface that has methods which get called depending on what menu option is selected

--- src/index.rb
+++ src/index.rb
@@ -1,15 +1,51 @@
 require "tty-prompt"
-prompt = TTY::Prompt.new
+require_relative("./data.rb")
+
+class Interface
+  attr_reader :prompt
+
+  def initialize
+    @prompt = TTY::Prompt.new
+  end
+
+  def welcome
+    puts "Welcome to the Pregnancy Calculator!"
+    user.name = prompt.ask("What is your name?", required: true)
+    puts "Hello #{user.name}."
+    sleep(1)
+  end
+
+  def main_menu
+    prompt.select("Please select an option from the following menu:") do |menu|
+      menu.default 1
+
+      menu.choice "Due Date Calculator", 1, -> {due_date_calculator}
+      menu.choice "View Disclaimer", 2, -> {disclaimer}
+      menu.choice "Exit", 3, -> {exit}
+    end
+  end
+
+  def due_date_calculator
+    puts "You chose Due Date Calculator - to be completed in the future"
+  end
+
+  def disclaimer
+    puts "You chose Disclaimer - to be completed in the future"
+  end
+
+  def exit
+    puts "Thank you for using the Pregnancy Calculator"
+  end
+end
 
 class User
     attr_accessor :name, :week_pregnant, :due_date
 end 
 
+
+
+# FEATURE 1 - Welcome message
 user = User.new
-
-puts "Welcome to the Pregnancy Calculator!"
-user.name = prompt.ask("What is your name?", required: true)
-puts "Hello #{user.name}."
-puts "Please select an option from the following menu:"
-
+interface = Interface.new
+welcome
 
